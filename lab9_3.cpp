ea8998d2e32c277c120ca34bcdb4097112cf3bdf
xchalarm <tharana4671@gmail.com>
2022-12-23 15:36:31 +0700

    LAB_3 COMPLETED

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,55 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    double high, bounty;
+string uc;
+cout << "Enter your age: ";
+cin >> age;
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> high;
+            if (high < 100.00)
+            {
+                cout << "Your character = "<< "Chopper";
+            }
+            else if(high < 180.00)
+            {
+                cout << "Your character = "<< "Usopp";
+            }
+            else 
+            {
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if (bounty > 1100000000.00) 
+                {
+                    cout << "Your character = "<< "Zoro";
+                }
+                else 
+                {
+                    cout << "Your character = "<<  "Sanji";
+                }
+            }
+    }
+    else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000.00)
+        {
+            cout << "Your character = "<< "Jinbe";
+        }
+        else
+        {
+            cout << "Your character = "<< "Franky";
+        }
+    }
+    else {
+        cout << "Your character = "<< "Brook";
+    }
+    return 0;
+}
